29f394e31b8c88368465ef89dc7e3f2603f2a8cc
AG <allig4t0r@gmail.com>
2024-04-08 18:46:22 +0300

    rotating log files

---
+++ log.py
@@ -0,0 +1,23 @@
+import config
+from logging import basicConfig, getLogger, Formatter, Logger
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+
+def log_init() -> Logger:
+    formatter = Formatter(fmt=config.LOG_FORMAT)
+    file_handler = RotatingFileHandler(filename=Path(config.LOG_PATH)/config.LOG_FILENAME, 
+                                       maxBytes=config.LOG_SIZE,
+                                        backupCount=config.LOG_FILECOUNT)
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(config.LOG_LEVEL)
+    
+    basicConfig(level=config.LOG_LEVEL, 
+                format=config.LOG_FORMAT,
+                handlers=[
+                    file_handler
+                ]
+    )
+
+    logger = getLogger(__name__)
+
+    return logger
