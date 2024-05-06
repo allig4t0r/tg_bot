9745a4c8ab3e779a67eb3650c9695dcf3b94d188
AG <allig4t0r@gmail.com>
2024-05-06 21:01:29 +0300

    may01_2024 initial prod

---
+++ states.py
@@ -0,0 +1,9 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class Whereami(StatesGroup):
+    main_menu_studios = State()
+    main_menu_kolya = State()
+    main_menu_admin = State()
+    text_input_studio_name = State()
+    text_input_tg_id = State()
+    text_input_key_id = State()
